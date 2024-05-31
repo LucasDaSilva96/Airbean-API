a51e8c95b99ba3124968cf5914746a6d15a81b88
LucasDaSilva96 <dasilvajunior881@gmail.com>
2024-05-31 19:51:51 +0200

    Created the about data, controller and route

---
+++ routes/aboutRoute.js
@@ -0,0 +1,7 @@
+const { sendAboutInfo } = require('../controllers/aboutController');
+const express = require('express');
+const router = express.Router();
+
+router.get('/about', sendAboutInfo);
+
+module.exports = router;
