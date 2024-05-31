c1647f7b2e7f7c86111c00d83d80db25c0fc6185
LucasDaSilva96 <dasilvajunior881@gmail.com>
2024-05-31 20:07:47 +0200

    Created the see delivery information, controller and route

---
+++ controllers/seeDeliveryStatsController.js
@@ -0,0 +1,23 @@
+const { OrderModel } = require('../models/orderModel');
+
+exports.getDeliveryInformation = async (req, res, next) => {
+  try {
+    const { orderID } = req.params;
+    if (!orderID) throw new Error('No order-id provided');
+
+    const order = await OrderModel.findById(orderID);
+
+    if (!order) throw new Error('No order found with the provided order-id');
+
+    res.status(200).json({
+      status: 'success',
+      message: 'Order successfully fetched',
+      data: order,
+    });
+  } catch (e) {
+    res.status(400).json({
+      status: 'fail',
+      message: e.message,
+    });
+  }
+};
--- routes/orderRoute.js
+++ routes/orderRoute.js
@@ -1,7 +1,10 @@
 const { createNewOrder } = require('../controllers/createOrderController');
+const {
+  getDeliveryInformation,
+} = require('../controllers/seeDeliveryStatsController');
 const express = require('express');
 const router = express.Router();
 
 router.post('/order', createNewOrder);
-
+router.get('/deliveryStats/:orderID', getDeliveryInformation);
 module.exports = router;
