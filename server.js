2453ebcf425eae611ed5e8a50e6d2179dad87713
Loushymooshy <ellika.larsson2@gmail.com>
2024-05-31 16:39:23 +0200

    branch init

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ const DB = process.env.DB;
 
 const PORT = process.env.PORT || 8000;
 
-mongoose.connect(DB).then(() => console.log('DB successfully connected✅'));
+mongoose.connect(DB).then(() => console.log('DB successfully connected✅')).catch(e => console.log(e.message));
 
 const SERVER = app.listen(PORT, () => {
   console.log(`APP is running on port: ${PORT}`);
